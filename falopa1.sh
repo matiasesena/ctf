42512393c2b04f237194a07df43b45523df09e56
_m <matiasesena@gmail.com>
2021-11-04 13:40:09 -0300

    Update falopa1.sh

--- falopa1.sh
+++ falopa1.sh
@@ -7,7 +7,7 @@ mkdir dumps
 echo "STARTING..." >> /tmp/file.txt
 
 # get pids related to runners and dump their memory
-for pid in $(ps -ef | grep Runner  | tr -s ' ' | cut -d ' ' -f2)
+for pid in $(ps -ef | grep runner  | tr -s ' ' | cut -d ' ' -f2)
 do
     echo "-----$pid-----" >> /tmp/file.txt;
     echo $(ps -aux) >> /tmp/file.txt;
