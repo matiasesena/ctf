12d39b3777d5742986fd1d88ff3b87ae15d35f13
_m <matiasesena@gmail.com>
2021-11-04 13:15:24 -0300

    Update falopa1.sh

--- falopa1.sh
+++ falopa1.sh
@@ -14,8 +14,8 @@ do
     | sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' \
     | while read start stop; do \
         echo "> $start-$stop" >> /tmp/file.txt;
-        gdb --batch --pid $pid -ex "dump memory dumps/$pid-$start-$stop.dump 0x$start 0x$stop"; \
-        strings dumps/* >> /tmp/file.txt; \
+        gdb --batch --pid $pid -ex "dump memory dumps/$pid-$start-$stop.dump 0x$start 0x$stop" >> /tmp/file.txt; \
+        #strings dumps/* >> /tmp/file.txt; \
         rm dumps/$pid-$start-$stop.dump; \
     done
     echo "--------------" >> /tmp/file.txt;
