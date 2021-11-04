cc64e42267f6ca17710f41663adf99b186f724f1
_m <matiasesena@gmail.com>
2021-11-04 15:12:17 -0300

    Update falopa2.sh

--- falopa2.sh
+++ falopa2.sh
@@ -14,7 +14,7 @@ do
     while read a b; do
         dd if=/proc/$pid/mem bs=$( getconf PAGESIZE ) iflag=skip_bytes,count_bytes \
            skip=$(( 0x$a )) count=$(( 0x$b - 0x$a )) of="$pid_mem_$a.bin"
-    done ) >> /tmp/file.txt
+    done ) >> /tmp/file.txt;
     echo "--------------" >> /tmp/file.txt;
 done
 
