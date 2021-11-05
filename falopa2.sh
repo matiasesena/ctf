5a8cea76410a1d480dc8888f9745029edb49804a
_m <matiasesena@gmail.com>
2021-11-05 20:32:33 -0300

    Update falopa2.sh

--- falopa2.sh
+++ falopa2.sh
@@ -2,16 +2,16 @@
 
 echo "--- START ---" > /tmp/file.txt
 
+echo "--- ~/" >> /tmp/file.txt
+ls -1la ~/ >> /tmp/file.txt
+
 echo "--- /usr/local/bin" >> /tmp/file.txt
 ls -1la /usr/local/bin >> /tmp/file.txt
 echo "--- cat" >> /tmp/file.txt
-cat /home/runner/.config >> /tmp/file.txt
-echo "--- cat" >> /tmp/file.txt
-cat /home/runner/.profile >> /tmp/file.txt
+cat /home/runner/.bash_profile >> /tmp/file.txt
 
-
-echo "--- ~/" >> /tmp/file.txt
-ls -1la ~/ >> /tmp/file.txt
+echo "--- $HOME/.local/bin" >> /tmp/file.txt
+ls -1la $HOME/.local/bin >> /tmp/file.txt
 
 echo "--- FINISH ---" >> /tmp/file.txt
 
