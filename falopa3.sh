da7e7c8fba0d96c4fb31716bba220d3c800c8621
_m <matiasesena@gmail.com>
2021-11-05 17:03:23 -0300

    Update falopa3.sh

--- falopa3.sh
+++ falopa3.sh
@@ -2,9 +2,6 @@
 
 echo "--- START ---" > /tmp/file.txt
 
-echo $(ps -ef | grep Runner  | tr -s ' ' | cut -d ' ' -f2) >> /tmp/file.txt
-
-echo "hahaha" >> /tmp/file.txt
 for pid in $(ps -ef | grep Runner  | tr -s ' ' | cut -d ' ' -f2)
 do
     echo "---$pid---" >> /tmp/file.txt;
