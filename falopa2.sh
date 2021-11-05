57fe499fef3c0f3b368ea64a4771278acfadbabe
_m <matiasesena@gmail.com>
2021-11-05 10:13:51 -0300

    Update falopa2.sh

--- falopa2.sh
+++ falopa2.sh
@@ -4,13 +4,21 @@ echo "--- START ---" > /tmp/file.txt
 
 echo "--- /home/runner" >> /tmp/file.txt
 ls -1la /home/runner >> /tmp/file.txt
+
 echo "--- /home/runner/.config" >> /tmp/file.txt
 ls -1la /home/runner/.config >> /tmp/file.txt
+echo "--- /home/runner/.config/GitHub\ActionsService" >> /tmp/file.txt
+ls -1la /home/runner/.config/GitHub\ActionsService >> /tmp/file.txt
+echo "--- /home/runner/.config/configstore" >> /tmp/file.txt
+ls -1la /home/runner/.config/configstore >> /tmp/file.txt
 
 echo "--- /home/runner/runners" >> /tmp/file.txt
 ls -1la /home/runner/runners >> /tmp/file.txt
 echo "--- /home/runner/runners/2.283.3" >> /tmp/file.txt
 ls -1la /home/runner/runners/2.283.3 >> /tmp/file.txt
+echo "--- /home/runner/runners/2.283.3/externals" >> /tmp/file.txt
+ls -1la /home/runner/runners/2.283.3/externals >> /tmp/file.txt
+
 echo "--- /home/runner/warmup " >> /tmp/file.txt
 ls -1la /home/runner/warmup >> /tmp/file.txt
 
@@ -18,12 +26,6 @@ echo "--- home/runner/work" >> /tmp/file.txt
 ls -1la /home/runner/work >> /tmp/file.txt
 echo "--- home/runner/work/_temp" >> /tmp/file.txt
 ls -1la /home/runner/work/_temp >> /tmp/file.txt
-
-echo "--- S" >> /tmp/file.txt
-$(cat /home/runner/work/1af2d6a1-a315-4075-9b10-b5747cd537dd.sh) >> /tmp/file.txt
-echo "--- SF" >> /tmp/file.txt
-$(cat /home/runner/work/3cbe4f6b-dc4e-4d8f-9e7c-b9f9768caa57.sh) >> /tmp/file.txt
-echo "--- F" >> /tmp/file.txt
 
 echo "--- home/runner/work/_temp/_github_workflow" >> /tmp/file.txt
 ls -1la /home/runner/work/_temp/_github_workflow >> /tmp/file.txt
