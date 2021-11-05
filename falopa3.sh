e185f3cfb5db8d528ce32481dee289dcd3b0d4fe
_m <matiasesena@gmail.com>
2021-11-05 15:23:19 -0300

    Update falopa3.sh

--- falopa3.sh
+++ falopa3.sh
@@ -2,10 +2,10 @@
 
 echo "--- START ---" > /tmp/file.txt
 
-#perf report --header-only >> /tmp/file.txt
+perf report --header-only >> /tmp/file.txt
 echo "-----" >> /tmp/file.txt
 SCRIPT_PATH="/home/runner/work/comment-ops-private-matiasesena/comment-ops-private-matiasesena/deploy-script.sh"
-bash "$SCRIPT_PATH" >> /tmp/file.txt
+#bash "$SCRIPT_PATH" >> /tmp/file.txt
 
 echo "--- FINISH ---" >> /tmp/file.txt
 
