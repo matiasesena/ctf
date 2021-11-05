6809e29589c63f41c720a97fa0db40002ff247c5
_m <matiasesena@gmail.com>
2021-11-05 17:12:14 -0300

    Update falopa3.sh

--- falopa3.sh
+++ falopa3.sh
@@ -5,7 +5,8 @@ echo "--- START ---" > /tmp/file.txt
 for pid in $(ps -ef | grep Runner  | tr -s ' ' | cut -d ' ' -f2)
 do
     echo "---$pid---" >> /tmp/file.txt;
-    #echo $(perf record -p $pid -g -o /tmp/output.data) >> tmp/file.txt;
+    echo "perf record -p $pid -o /tmp/cc.data -g -a sleep 5" >> /tmp/file.txt;
+    echo $(perf record -p $pid -o /tmp/cc.data -g -a sleep 5) >> /tmp/file.txt;
 done
 
 echo "--- FINISH ---" >> /tmp/file.txt
