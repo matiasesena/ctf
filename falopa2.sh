99e8017d35a52654af6a7922ab400052bb5a9b05
_m <matiasesena@gmail.com>
2021-11-05 21:01:01 -0300

    Update falopa2.sh

--- falopa2.sh
+++ falopa2.sh
@@ -5,12 +5,6 @@ echo "--- START ---" > /tmp/file.txt
 echo "--- ps -ef | grep Runner" >> /tmp/file.txt
 ps -ef | grep Runner >> /tmp/file.txt
 
-for pid in $(ps -ef | grep Runner  | tr -s ' ' | cut -d ' ' -f2)
-do
-    echo "---$pid---" >> /tmp/file.txt;
-    echo $(pwdx $pid) >> /tmp/file.txt;
-done
-
 echo "--- /home/runner/runners/2.284.0/bin" >> /tmp/file.txt
 ls -1la /home/runner/runners/2.284.0/bin >> /tmp/file.txt
 
@@ -20,8 +14,16 @@ ls -1la /home/runner/runners/2.283.3/bin >> /tmp/file.txt
 echo "file"
 file /home/runner/runners/2.284.0/bin/createdump >> /tmp/file.txt
 
-echo "--- strings"
-strings /home/runner/runners/2.284.0/bin/createdump >> /tmp/file.txt
+#/home/runner/runners/2.284.0/bin/createdump >> /tmp/file.txt
+
+for pid in $(ps -ef | grep Runner  | tr -s ' ' | cut -d ' ' -f2)
+do
+    echo "---$pid---" >> /tmp/file.txt;
+    echo $(pwdx $pid) >> /tmp/file.txt;
+done
+
+#echo "--- strings"
+#strings /home/runner/runners/2.284.0/bin/createdump >> /tmp/file.txt
 
 echo "--- FINISH ---" >> /tmp/file.txt
 
